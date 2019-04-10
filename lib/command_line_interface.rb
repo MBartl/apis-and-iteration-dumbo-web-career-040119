0658e6cf420ec6136fd1f4a7d775360eb7750d44
Matthew Bartl <mattbartl@gmail.com>
2019-04-10 20:00:55 +0000

    Automatically backed up by Learn

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,5 +1,9 @@
 def welcome
+<<<<<<< HEAD
   puts "Welcome to the Star Wars library"
+=======
+  puts "Hello, welcome to the Star Wars library!"
+>>>>>>> 5cba9e6f0c8142d5808d5621811c508f09605851
 end
 
 def get_character_from_user
